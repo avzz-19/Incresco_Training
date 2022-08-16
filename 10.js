eb12b868b1584946b14b8fc50e85372f1a461492
Avi Shah <avishah@Avis-MacBook-Air.local>
2022-08-16 21:35:32 +0530

    addition

--- 10.js
+++ 10.js
@@ -6,7 +6,11 @@ function multiply() {
        return acc*cur;
    })
 }
+//addition
 console.log(multiply(2,6,8,10));
-
-
-//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
+function add(){
+    let args=Array.from(arguments);
+    return args.reduce(function(acc,cur){
+    return acc+cur;})
+}
+console.log(add(2,5));
